45197d4226a050aa01dfff80bf1e3a11722260b1
Andrew Southern <whoamihealay@gmail.com>
2022-03-22 15:28:45 -0400

    added todo model

---
+++ server/models/todoModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const todoSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    },
+    text: {
+      type: String,
+      required: [true, 'Please add a text value']
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('Todo', todoSchema)
